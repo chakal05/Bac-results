0a7a0c0a9de3aa0bccfd43ba90ec5e863a2da193
chakal05 <chakirrasta@gmail.com>
2019-08-04 19:32:51 +0200

    login + retrieve from db

---
+++ php/connection.php
@@ -0,0 +1,8 @@
+<?php
+$link = mysqli_connect("localhost", "root", "chakal10", "bacResults");
+if(mysqli_connect_error()) {
+    die("database error".mysqli_connect_error());
+    }
+
+
+?>
--- php/index.php
+++ php/index.php
@@ -1,10 +1,67 @@
 <?php
 
-/* 
 
-    This part for php
 
-*/
+    session_start();
+  $error = "";
+  $missing = "";
+  // Logging out the user
+  if (array_key_exists('logout', $_GET) == '1') {
+        header("location: index.php");
+        session_unset();
+        session_destroy();
+        exit;
+    } 
+    
+    if (isset( $_SESSION['id'])) {
+    
+    header("Location: results.php");
+    
+    } else if (array_key_exists('submit', $_POST))  {
+    
+    include("./connection.php");
+   
+    
+    if(!$_POST['nom']) {
+        $missing.= " Entrez votre nom <br>";
+    } 
+    
+    
+    if(!$_POST['pass']) {
+        $missing.= "Mot de passe absent<br>";
+    } 
+    if($missing != "") {
+        $error .= "Les espaces suivants doivent etre remplis: <br>" .$missing;
+    }
+    if($error != "") {
+      
+        $error = '<div class="alert alert-danger" role="alert"><p> Il y a des erreurs dans votre formulaire </p>' . $error . '</div>';
+        
+    } else {
+        
+    
+    $query = "SELECT * FROM `resultat` WHERE nom = '".mysqli_real_escape_string($link, $_POST['nom'])."' AND numExam ='".mysqli_real_escape_string($link, $_POST['pass'])."' LIMIT 1";
+     
+    $result = mysqli_query($link, $query);
+    $row =  mysqli_fetch_array($result);
+    
+    if (isset($row)) {
+    $_SESSION['id']=$row['id'];
+    header("Location: results.php");
+    exit;
+    } else {
+        
+        $error = '<div class="alert alert-danger" role="alert"><p>Email et/ou mot de passe non reconnu. Controllez svp</p></div>';
+        
+         }
+        
+    } 
+
+
+    }
+   
+
+
 
 ?>
 
@@ -24,16 +81,16 @@
     <div class="content">
     <div class="logo">
     <img src="../images/menfop1.jpg"></div>
-    <form>
+    <form method="POST">
   <div class="form-group">
-     <input type="text" class="form-control" id="nom" aria-describedby="emailHelp" placeholder="Votre nom">
+     <input type="text" class="form-control" id="nom" name="nom" aria-describedby="emailHelp" placeholder="Votre nom">
  </div>
   <div class="form-group">
-    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Numéro d'inscription">
+    <input type="password" class="form-control" id="exampleInputPassword1" name="pass" placeholder="Numéro d'inscription">
   </div>
-  <button type="submit" class="btn btn-primary">Connexion</button>
+  <button type="submit" name="submit" class="btn btn-primary">Connexion</button>
 </form>
-
+    <div id="error"> <?php echo $error;  ?></div>
     </div>
 
 
